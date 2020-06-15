7b27828ea989a60dd8c9b0f98734455b675f4c19
JWaltuch <jenna.waltuch@gmail.com>
2020-06-15 17:08:08 -0400

    handles when user does not enter filename

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ let storage = multer.diskStorage({
     if (req.body.filename !== '') {
       cb(null, req.body.filename + '.mov');
     } else {
-      cb(null, file.originalname + '.mov');
+      cb(null, file.originalname);
     }
   },
 });
