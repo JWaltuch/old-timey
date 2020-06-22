d6f68137468aec4209ac55e0aeff072842c2caed
JWaltuch <jenna.waltuch@gmail.com>
2020-06-22 22:38:20 +0100

    loops through all videos and converts to BW

--- convert.bash
+++ convert.bash
@@ -1,3 +1,9 @@
-INPUT_VIDEO=$1 TYPE=$2
+# INPUT_VIDEO=$1 TYPE=$2
 
-ffmpeg -i $INPUT_VIDEO -vf hue=s=0 BW/${INPUT_VIDEO%.*}.mov
+# ffmpeg -i $INPUT_VIDEO -vf hue=s=0 BW/${INPUT_VIDEO%.*}.mov
+
+
+for INPUT_VIDEO in /var/old-timey/videos/* ; do
+  vidname=${INPUT_VIDEO:22}
+  ffmpeg -i $INPUT_VIDEO -vf hue=s=0 /var/old-timey/videos/BW/${vidname%.*}.mov
+done
