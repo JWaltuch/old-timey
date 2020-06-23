cd0972e42c14eaa2182380e9455e16ea37a06c16
JWaltuch <jenna.waltuch@gmail.com>
2020-06-23 03:05:28 +0100

    updated BW files to have name with BW in bash script

--- convert.bash
+++ convert.bash
@@ -3,7 +3,13 @@
 # ffmpeg -i $INPUT_VIDEO -vf hue=s=0 BW/${INPUT_VIDEO%.*}.mov
 
 
-for INPUT_VIDEO in /var/old-timey/videos/* ; do
-  vidname=${INPUT_VIDEO:22}
-  ffmpeg -i $INPUT_VIDEO -vf hue=s=0 /var/old-timey/videos/BW/${vidname%.*}.mov
-done
+# for INPUT_VIDEO in /var/old-timey/videos/* ; do
+#   vidname=${INPUT_VIDEO:22}
+#   ffmpeg -i $INPUT_VIDEO -vf hue=s=0 /var/old-timey/videos/BW/${vidname%.*}.mov
+# done
+
+INPUT_VIDEO=$1
+
+vidname=${INPUT_VIDEO:22}
+
+ffmpeg -v quiet -stats -i $INPUT_VIDEO -vf hue=s=0 /var/old-timey/videos/BW/${vidname%.*}_BW.mov
